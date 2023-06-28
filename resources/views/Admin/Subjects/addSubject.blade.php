e66bad5b040b435fde0b74ddf9f7965f3f2f1269
AA1401 Vihanga Herath <94732155+VihangaManodyaHerath@users.noreply.github.com>
2023-06-28 20:19:55 +0530

    Update addSubject.blade.php

--- resources/views/Admin/Subjects/addSubject.blade.php
+++ resources/views/Admin/Subjects/addSubject.blade.php
@@ -22,7 +22,7 @@
                                 <b>Subject ID</b>
                             </th>
                             <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                <b>Subject Name</b>
+                                <b>Subject Name</b> //Subject Name is taken to create the Subject folder
                             </th>
                         </tr>
                         </thead>
